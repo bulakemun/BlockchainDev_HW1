5f7d6b0ccef205107a5b9bfdc3d13ccee966e498
Efe Kamil Bayrak <70903320+bulakemun@users.noreply.github.com>
2024-12-07 12:25:45 +0300

    Added the print functions of user and pool balances.

--- main.js
+++ main.js
@@ -9,14 +9,18 @@ import figlet from "figlet";
 const data = fs.readFileSync('data.json', 'utf8')
 let userBalanceA = JSON.parse(data).userBalance.tokenA;
 let userBalanceB = JSON.parse(data).userBalance.tokenB;
+let poolBalanceA = JSON.parse(data).pool.tokenA;
+let poolBalanceB = JSON.parse(data).pool.tokenB;
+let ratio = JSON.parse(data).pool.K;
+console.log(ratio);
 
 program
     .description("First Homework")
     .version("1.0.0")
     .command('start')
     .action(() => {
-        console.log(chalk.yellow(figlet.textSync("Homework 1", {horizontalLayout: "full"})));
-        console.log(userBalanceA);
+        console.log(chalk.yellow(figlet.textSync("ITU Blockchain Homework 1", {horizontalLayout: "fitted"})));
+        // console.log(userBalanceA);
         inquirer
           .prompt([
             {
@@ -39,7 +43,9 @@ program
                     ]);
                 case "Swap":
                 case "Havuz Durumunu Görüntüle":
+                    console.log(chalk.green(`Pool Balance A: ${poolBalanceA}\nPool Balance B: ${poolBalanceB}\nRatio: ${ratio}`));
                 case "Kullanıcı Bakiyesini Görüntüle":
+                    console.log(chalk.green(`User Balance A: ${userBalanceA}\nUser Balance B: ${userBalanceB}`));
                 case "Çıkış":
                     break;
             }
