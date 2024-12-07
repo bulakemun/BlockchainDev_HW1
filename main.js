80f22750e2423084df041165fc8be78f6a8db0f3
Efe Kamil Bayrak <70903320+bulakemun@users.noreply.github.com>
2024-12-07 11:04:38 +0300

    Implemented readFile correctly, with a synchronous function.

--- main.js
+++ main.js
@@ -6,15 +6,9 @@ import inquirer from 'inquirer';
 import chalk from "chalk";
 import figlet from "figlet";
 
-fs.readFile("data.json", (error, data) => {
-    if (error) {
-      console.error(error);
-      throw error;
-    }
-    const user = JSON.parse(data);
-    // console.log(user);
-  });
-
+const data = fs.readFileSync('data.json', 'utf8')
+let userBalanceA = JSON.parse(data).userBalance.tokenA;
+let userBalanceB = JSON.parse(data).userBalance.tokenB;
 
 program
     .description("First Homework")
@@ -22,17 +16,33 @@ program
     .command('start')
     .action(() => {
         console.log(chalk.yellow(figlet.textSync("Homework 1", {horizontalLayout: "full"})));
+        console.log(userBalanceA);
         inquirer
           .prompt([
             {
               type: "list",
-              name: "name",
+              name: "choice",
               message: "Choose one:",
               choices: ["Likidite Ekle", "Swap", "Havuz Durumunu Görüntüle", "Kullanıcı Bakiyesini Görüntüle", "Çıkış"]
             },
           ])
-          .then((answers) => {
-            console.log(chalk.green(answers.name));
+          .then((result) => {
+            switch(result.choice) {
+                case "Likidite Ekle":
+                    inquirer.prompt([
+                        {
+                            type: "list",
+                            name: "num1",
+                            message: "Which coin would you like to add liq. to?:",
+                            choices: ["tokenA", "tokenB"]
+                        },
+                    ]);
+                case "Swap":
+                case "Havuz Durumunu Görüntüle":
+                case "Kullanıcı Bakiyesini Görüntüle":
+                case "Çıkış":
+                    break;
+            }
           });
       });
 program.parse(process.argv);
